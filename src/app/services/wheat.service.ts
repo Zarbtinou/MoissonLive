bdddc27425f8f5d88d02d1e095c5241324244f8a
Arthur Jouanneau <jouanneauarthur@gmail.com>
2019-06-06 01:05:11 +0200

    Create wheat Service

---
+++ src/app/services/wheat.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Wheat } from '../class/wheat' ;
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WheatService {
+  private client:HttpClient;
+
+  constructor( http:HttpClient ) {
+    this.client = http;
+   }
+
+   public getAll():Observable<Wheat[]>{
+
+    let obs:Observable<any> = this.client.get("https://api.capgrain.com/wheat-observations");
+    let treatment = (data:any) => {
+      return data['hydra:member'] as Wheat[];
+    };
+
+    return obs.pipe(map(treatment));
+   }
+}
