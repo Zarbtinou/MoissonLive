185a50f8a0f310546e8356dd699aacefc257ba36
Quentin Moreau <qdr.moreau@yahoo.fr>
2019-06-06 00:17:04 +0200

    Ad service barley

---
+++ src/app/barley.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Barley } from './barley';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BarleyService {
+
+  private client:HttpClient;
+
+  constructor( http:HttpClient) {
+    this.client = http;
+  }
+
+  public getAll():Observable<Barley[]>{
+
+    let obs:Observable<any> = this.client.get('assets/barley.json');
+
+    let treatment = (data:any) => {
+      return data['hydra:member'] as Barley[];
+    };
+
+    return obs.pipe( map(treatment) );
+  }
+}
